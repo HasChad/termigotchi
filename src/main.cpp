7464983e7a301fa6ebde058d93d1abc9949ed490
HasChad <troll.yahya@gmail.com>
2025-11-26 11:45:23 +0300

    - first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Pet {
+    string name;
+    int health = 100;
+    int energy = 100;
+    int moral = 100;
+    int hunger = 100;
+};
+
+int main() {
+    cout << "Hello World";
+
+    return 0;
+}
